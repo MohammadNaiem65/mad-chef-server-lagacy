a662361c46dec161cba362529f302c04de647092
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-03 04:06:05 +0600

    added the chefs details and made a route for that

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require("express")
+const express = require("express");
+const chefs = require("./data/chefs.json");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res)=>{
-    res.send("Hello World")
-})
+app.get("/chefs", (req, res) => {
+	res.send(chefs);
+});
 
-app.listen(port, ()=>{
-    console.log(`Server running on ${port}`)
-})
+app.listen(port, () => {
+	console.log(`Server running on ${port}`);
+});
