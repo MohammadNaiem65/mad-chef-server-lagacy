0a3738e85e01897d2b77bc263495e1425e5d1765
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-03 13:55:52 +0600

    added /chef/name route and id to all the chef objects

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const chefs = require("./data/chefs.json");
+const cors = require("cors");
 const app = express();
 const port = 5000;
+
+app.use(cors())
 
 app.get("/chefs", (req, res) => {
 	res.send(chefs);
 });
+
+app.get("/chefs/name", (req, res) =>{
+    const names = [];
+    chefs.forEach(chef => {
+        let cName = {
+            id: chef.id,
+            name: chef.name,
+            availableRecipes: chef.availableRecipes.length
+        }
+        names.push(cName)
+    })
+    res.send(names)
+})
 
 app.listen(port, () => {
 	console.log(`Server running on ${port}`);
