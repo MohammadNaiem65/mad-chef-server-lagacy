c6f53e21ff6e12ed8955ae89e28472148d86c722
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-03 15:24:54 +0600

    updated the json file

--- index.js
+++ index.js
@@ -4,29 +4,29 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(cors())
+app.use(cors());
 
 app.get("/chefs", (req, res) => {
 	res.send(chefs);
 });
 
-app.get("/chefs/name", (req, res) =>{
-    const names = [];
-    chefs.forEach(chef => {
-        let cName = {
-            id: chef.id,
-            name: chef.name,
-            availableRecipes: chef.availableRecipes.length
-        }
-        names.push(cName)
-    })
-    res.send(names)
-})
+app.get("/chefs/name", (req, res) => {
+	const names = [];
+	chefs.forEach((chef) => {
+		let cName = {
+			id: chef.id,
+			name: chef.name,
+			availableRecipes: chef.availableRecipes.length,
+		};
+		names.push(cName);
+	});
+	res.send(names);
+});
 
-app.get("/chefs/chef/:id", (req, res) =>{
-    const chef = chefs.find(chef=> chef.id == req.params.id)
-    res.send(chef)
-})
+app.get("/chefs/chef/:id", (req, res) => {
+	const chef = chefs.find((chef) => chef.id == req.params.id);
+	res.send(chef);
+});
 
 app.listen(port, () => {
 	console.log(`Server running on ${port}`);
