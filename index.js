4027eb9a5c19d7ff4a257d5cde8757f606aad4fc
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-05 07:49:51 +0600

    added the /reviews route and review.json file

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
-const chefs = require("./data/chefs.json");
 const cors = require("cors");
+const chefs = require("./data/chefs.json");
+const reviews = require("./data/reviews.json");
+
 const app = express();
 const port = 5000;
 
@@ -28,10 +30,14 @@ app.get("/chefs/chef/:id", (req, res) => {
 	res.send(chef);
 });
 
-app.get("/chefs/chef/recipes/:id", (req, res)=>{
-    const chef = chefs.find((chef) => chef.id == req.params.id);
-    res.send(chef.recipes);
-})
+app.get("/chefs/chef/recipes/:id", (req, res) => {
+	const chef = chefs.find((chef) => chef.id == req.params.id);
+	res.send(chef.recipes);
+});
+
+app.get("/reviews", (req, res) => {
+	res.send(reviews);
+});
 
 app.listen(port, () => {
 	console.log(`Server running on ${port}`);
