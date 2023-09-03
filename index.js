1900646fcfcae5aeddaa6d37d8498ef89ac05e83
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2023-09-03 14:23:19 +0600

    connected db

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-const express = require("express");
-const cors = require("cors");
-const chefs = require("./data/chefs.json");
-const reviews = require("./data/reviews.json");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const chefs = require('./data/chefs.json');
+const reviews = require('./data/reviews.json');
 
 const app = express();
 const port = 5000;
-
 app.use(cors());
 
-app.get("/chefs", (req, res) => {
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@madchef.30s9xap.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get('/chefs', (req, res) => {
 	res.send(chefs);
 });
 
-app.get("/chefs/name", (req, res) => {
+app.get('/chefs/name', (req, res) => {
 	const names = [];
 	chefs.forEach((chef) => {
 		let cName = {
@@ -25,23 +52,23 @@ app.get("/chefs/name", (req, res) => {
 	res.send(names);
 });
 
-app.get("/chefs/chef/:id", (req, res) => {
+app.get('/chefs/chef/:id', (req, res) => {
 	const chef = chefs.find((chef) => chef.id == req.params.id);
 	res.send(chef);
 });
 
-app.get("/chefs/chef/recipes/:id", (req, res) => {
+app.get('/chefs/chef/recipes/:id', (req, res) => {
 	const chef = chefs.find((chef) => chef.id == req.params.id);
 	res.send(chef.recipes);
 });
 
-app.get("/top-chefs", (req, res) => {
+app.get('/top-chefs', (req, res) => {
 	const chefsCopy = chefs;
 	const sortedValues = chefsCopy.sort((a, b) => b.rating - a.rating);
 	res.send(sortedValues);
 });
 
-app.get("/reviews", (req, res) => {
+app.get('/reviews', (req, res) => {
 	res.send(reviews);
 });
 
